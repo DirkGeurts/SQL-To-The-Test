58a4f7192a2173b8445ebe5fcbc7bb0815f1767f
Dirk Geurts <Dirk.T.Geurts@gmail.com>
2019-05-03 14:54:56 +0200

    change

--- account.php
+++ account.php
@@ -24,57 +24,32 @@ if(!isset($_SESSION['email'])){
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>edit account</title>
-    <link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <head>
+        <meta charset="UTF-8">
+        <link rel='stylesheet' type='text/css' href='style.css' />
+        <title>Update Account</title>
+    </head>
+    <body>
 
-    <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="style.css">
 
-  </head>
-  <body>
-      
-    <div class="containter">
-        <div class="page jumbotron d-flex justify-content-center">
-            <form action="auth.php" method="POST">
-                <div class="row d-flex justify-content-start ml-5">
-                    <div class="form-group m-3 col-md-5">
-                        <label class="text-white font-weight-bold">first name</label>
-                        <input type="text" name="firstname" required class="form-control" placeholder="Firstname" value="<?php echo $_SESSION['firstname']?>">
-                    </div>
-                
-                    <div class="form-group m-3 col-md-5">
-                        <label class="text-white font-weight-bold">last name</label>
-                        <input type="text" name="lastname" required class="form-control" placeholder="Last name" value="<?php echo $_SESSION['lastname']?>">
-                    </div>
-                    <div class="form-group m-3 col-md-3">
-                        <label class="text-white font-weight-bold">Username</label>
-                        <input type="text" name="username" required class="form-control" placeholder="Enter username" value="<?php echo $_SESSION['username']?>">
-                    </div>
-                    <div class="form-group m-3 col-md-5">
-                        <label class="text-white font-weight-bold">Email address</label>
-                        <input type="email" name="email" required class="form-control" placeholder="Enter email" value="<?php echo $_SESSION['email']?>">
-                    </div>
-                </div>
-                
-                <div class="row d-flex justify-content-center mt-5">
-                    <div class="form-group">
-                        <button type="submit" name="update" class="btn btn-primary">UPDATE</button>
-                    </div>
-                </div>
-                <div class="row d-flex justify-content-center mt-5">
-                    <div class="form-group">
-                    <input type="submit" name="logout" value="log out">
-                    </div>
-                </div>
+<h1>Update Account</h1>
+        <div class="container">
+        <form action="auth.php" method="POST">
+            
+            <input type="text" id="username" name="username" placeholder="Username" required><br>
+            
+            <input type="text" id="firstname" name="firstname" placeholder="Firstname" required><br>
+            
+            <input type="text" id="lastname" name="lastname" placeholder="Lastname" required><br>
+            
+            <input type="email" id="email" name="email" placeholder="Email" required><br>
+            
+            <input class="registerbtn" type="submit" name="update" value="Update"></button> 
+            
+        </form>
+        <div>
+        <form action="auth.php" method="POST">
+            <input type="submit" name="logout" value="logout">
             </form>
-        </div>
-    </div>
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+    </body>
 </html>
